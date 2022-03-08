55fb97ec27b3b14e78208b956e037b62a00587b8
SupaSibs <omareeto2012@hotmail.com>
2022-03-08 18:42:55 +0200

    dd

---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+<main class="am-card-4">
+  <img id="equil" src="images/image-equilibrium.jpg" alt="Equillibrium"> 
+  <p>Equilibrium #3429</p>                                                                                             
+  <p>Our Equilibrium collection promotes balance and calm.</p>                                                    
+  <span class="claim">
+  <p><img id="ether" src="images/icon-ethereum.svg" alt="Ethereum">0.041 ETH</p>
+  <p><img id="time" src="images/icon-clock.svg" alt="time"> 3 days left</p>
+</span>
+  <hr>
+  <footer><p><img id="jules" src="images/image-avatar.png" alt="Jules Wyvern">Creation of Jules Wyvern</p></footer>
+</main>
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+    Coded by <a href="#">Your Name Here</a>.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
